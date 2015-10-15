d1c78c0dd17eec3f751421e966de7ce8aa3bc8a8
AllanRomanato <allan.romanato@gmail.com>
2015-10-15 09:36:27 -0300

    Create DBHelper.java

---
+++ DBHelper.java
@@ -0,0 +1,38 @@
+package br.com.home.adaptercustomizado;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by allanromanato on 6/2/15.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+    static final String BANCO = "carro.db";
+    static final int VERSAO = 1;
+    static final String TABELA = "carros";
+    static final String ID = "_id";
+    static final String FOTO = "foto";
+    static final String NOME = "nome";
+    static final String DONO = "autor";
+
+    public DBHelper(Context context){
+        super(context, BANCO, null, VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table " + TABELA
+                + "(" + ID + " integer primary key autoincrement, "
+                + NOME + " text, "
+                + DONO + " text, "
+                + FOTO + " blob )";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABELA);
+        onCreate(db);
+    }
+}
